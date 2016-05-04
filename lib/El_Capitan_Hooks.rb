4467ab758abc20fdd703606ccb6c76ae939c376e
Fran Romero <fj.romero93@gmail.com>
2016-05-04 13:28:05 +0200

    Hours
    You can see your current week hours (My account)

--- lib/El_Capitan_Hooks.rb
+++ lib/El_Capitan_Hooks.rb
@@ -1,5 +1,10 @@
 class ElCapitanHooks < Redmine::Hook::ViewListener
 
-    render_on :view_users_form , :partial => 'user/hours'
+    #User form
+    render_on :view_users_form , :partial => 'user/form'
 
+    #User account
+    #render_on :view_account_right_bottom , :partial => 'user/myaccount'
+
+    render_on :view_my_account , :partial => 'user/myaccount'
 end
--- lib/users_path.rb
+++ lib/users_path.rb
@@ -4,10 +4,24 @@ module ElCapitan
     module UsersPatch
       def self.included(base)
         base.class_eval do
+          unloadable
+
           safe_attributes 'weekly_hours'
           safe_attributes 'tracking'
 
           validates :weekly_hours , :numericality => {:greater_than_or_equal_to => 0}
+
+          #@brief Returns sum of hours that a user has spent on a week.
+          #@param [in] week if nil, it uses current week, otherwise it will use the week param
+          #return Total hours of the week
+          def TotalWeekHours(week=nil)
+            if week.nil?
+              current_week = Time.now.strftime('%W').to_i
+              return TimeEntry.where(:user_id => self.id , :tweek =>  current_week).sum(:hours)
+            else
+              return TimeEntry.where(:user_id => self.id , :tweek =>  week).sum(:hours)
+            end
+          end
         end
       end
     end
